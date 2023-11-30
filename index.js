9559804d0dffc4b6906bb92a37741396e2f31db1
mycomike <mycoberago@gmail.com>
2023-11-30 11:34:55 -0600

    Final solution accepted

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var cors = require('cors');
+var bodyParser = require('body-parser');
 require('dotenv').config()
+var formidable = require('formidable');
 
 var app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -12,6 +15,18 @@ app.get('/', function (req, res) {
 });
 
 
+app.post('/api/fileanalyse', async (req,res) => {
+  var form = new formidable.IncomingForm();
+  form.parse(req, function (err, fields, files) {
+    console.dir(files.upfile[0].mimetype);
+    let obj = {};
+    obj.name = files.upfile[0].originalFilename;
+    obj.type = files.upfile[0].mimetype;
+    obj.size = files.upfile[0].size;
+    res.json(obj);
+  });
+  
+})
 
 
 const port = process.env.PORT || 3000;
